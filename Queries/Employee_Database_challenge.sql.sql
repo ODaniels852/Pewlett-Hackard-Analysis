ed5073fa9898112bc4de1e9bcef4e9dd9100c49f
Onassis Daniels <onassis.daniels@gmail.com>
2022-08-11 19:49:57 -0700

    adding the query for retirement_titles

--- Queries/Employee_Database_challenge.sql.sql
+++ Queries/Employee_Database_challenge.sql.sql
@@ -37,3 +37,17 @@ ON (t.emp_no = e.emp_no)
 WHERE (t.to_date = '9999-01-01') 
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+-- adding query for retirement_titles
+SELECT e.emp_no, 
+    e.first_name, 
+    e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date  
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
